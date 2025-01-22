cd2a19244cefeb8a29347bfc0e53560c47cece5f
jonghyeonkimjh <whdgus4598@gmail.com>
2025-01-22 16:03:23 +0900

    Hyper Race Game 구현

---
+++ Assets/Scripts/Gas.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gas : MonoBehaviour
+{
+    public float gas = 30.0f;
+    public float moveSpeed = 10.0f;
+    public float limitPosition = 20.0f;
+    
+    private RandomGas _randomGas;
+
+    private void OnDisable()
+    {
+        _randomGas.ReturnToPool(gameObject);
+    }
+
+    private void Start()
+    {
+        _randomGas = gameObject.transform.parent.GetComponent<RandomGas>();
+    }
+
+    private void Update()
+    {
+        if (!gameObject.activeSelf) return;
+
+        transform.position -= moveSpeed * Time.deltaTime * Vector3.forward;
+        if (transform.position.z <= -limitPosition)
+        {
+            _randomGas.ReturnToPool(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<CarController>().CurrentGas += gas;
+        _randomGas.ReturnToPool(gameObject);
+    }
+}
